624a22ccdee83eba1054e22ecc13a012832faf95
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2016-11-22 21:07:41 -0800

    database task config

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,14 @@
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    mongojs = require('mongojs'),
+    db = mongojs(MONGODB_URI, ['tasks']);
 
 router.get('/tasks', function(req, res, next) {
-    res.send('TASK API');
+    db.tasks.find(function(err, tasks) {
+        if (err) res.send(err);
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
+console.log(MONGODB_URI);
